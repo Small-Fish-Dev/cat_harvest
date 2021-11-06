d5e7006bbae453e3e1fa222946b8e30d22aead2b
yuberee <59583743+yuberee@users.noreply.github.com>
2021-11-06 22:24:16 +0100

    innit

---
+++ code/Game.cs
@@ -0,0 +1,35 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Cat_Harvest
+{
+
+	public partial class Game : Sandbox.Game
+	{
+		public Game()
+		{
+			if ( IsServer )
+			{
+
+				new HarvestHUD();
+
+			}
+
+		}
+
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new HarvestPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+
+}
---
+++ code/HarvestHUD.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox.UI;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace Cat_Harvest
+{
+	/// <summary>
+	/// This is the HUD entity. It creates a RootPanel clientside, which can be accessed
+	/// via RootPanel on this entity, or Local.Hud.
+	/// </summary>
+	public partial class HarvestHUD : Sandbox.HudEntity<RootPanel>
+	{
+		public HarvestHUD()
+		{
+			if ( IsClient )
+			{
+				RootPanel.SetTemplate( "/HarvestHUD.css" );
+			}
+		}
+	}
+
+}
