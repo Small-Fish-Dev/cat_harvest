eeb40b78173889afa0131db257be63a8b07ae79b
yuberee <59583743+yuberee@users.noreply.github.com>
2023-12-14 22:04:22 +0100

    Pretty good first person controller

--- code/Player/FirstPersonController.cs
+++ code/Player/FirstPersonController.cs
@@ -1,88 +1,73 @@
-using Sandbox;
-using System.Drawing;
-using System.Linq;
-using System.Runtime;
-
 public class FirstPersonController : Component
 {
-	[Property] public GameObject Body { get; set; }
+	[Property] public CharacterController CharacterController { get; set; }
 	[Property] public GameObject Camera { get; set; }
-	[Property] public float WalkSpeed { get; set; } = 200f;
-	[Property] public float RunSpeed { get; set; } = 400f;
+	[Property] public float WalkSpeed { get; set; } = 120f; // How fast we walk
+	[Property] public float RunSpeed { get; set; } = 250f; // How fast we run
+	[Property] public float JumpStrength { get; set; } = 300f; // How high we jump
 
-	public Angles EyeAngles;
-	public Rotation EyeRotation => EyeAngles.ToRotation();
+	public Angles EyeAngles { get; private set; }
 	public Vector3 WishVelocity { get; private set; }
-	public CharacterController Controller => Body?.Components.GetAll<CharacterController>().FirstOrDefault() ?? null;
-	public Vector3 Gravity => Vector3.Down * 800;
 
-	protected override void OnEnabled()
+	protected override void OnEnabled() // Called as soon as the component gets enabled
 	{
 		base.OnEnabled();
 
 		if ( Camera != null )
-		{
-			EyeAngles = Camera.Transform.Rotation.Angles();
-			EyeAngles.roll = 0;
-		}
+			EyeAngles = Camera.Transform.Rotation.Angles(); // Starting eye angles set to whatever the camera is
 	}
 
-	protected override void OnUpdate()
+	protected override void OnUpdate() // Called every frame
 	{
 		if ( Camera != null )
 		{
-			EyeAngles.pitch += Input.MouseDelta.y * 0.1f; // Up-Down
-			EyeAngles.yaw -= Input.MouseDelta.x * 0.1f; // Left-Right
-			EyeAngles.roll = 0;
-
-			Camera.Transform.Rotation = EyeRotation;
+			EyeAngles += Input.AnalogLook * 5f; // Rotate our view angles
+			Camera.Transform.Rotation = EyeAngles.ToRotation(); // Set the camera's rotation based off of our eye angles
 		}
 	}
 
-	protected override void OnFixedUpdate()
+	protected override void OnFixedUpdate() // Called every tick
 	{
-		BuildWishVelocity();
+		ComputeWishVelocity();
 
-		if ( Controller != null )
+		if ( CharacterController != null )
 		{
-			if ( Controller.IsOnGround )
-			{
-				Controller.Velocity = Controller.Velocity.WithZ( 0 );
-				Controller.Accelerate( WishVelocity );
-				Controller.ApplyFriction( 4.0f );
-			}
-			else
-			{
-				Controller.Velocity += Gravity * Time.Delta * 0.5f;
-				Controller.Accelerate( WishVelocity.ClampLength( 50 ) );
-				Controller.ApplyFriction( 0.1f );
-			}
-
-			Controller.Move();
-
-			if ( !Controller.IsOnGround )
-				Controller.Velocity += Gravity * Time.Delta * 0.5f;
-			else
-				Controller.Velocity = Controller.Velocity.WithZ( 0 );
+			ComputeHelper();
+			ComputeJump();
 		}
 	}
 
-	public void BuildWishVelocity()
+	public void ComputeWishVelocity()
 	{
-		var eyeRotation = EyeRotation;
+		var playerYaw = Rotation.FromYaw( EyeAngles.yaw ); // Horizontal movement only needs yaw based off of where we're looking
+		var direction = Input.AnalogMove * playerYaw; // Rotate your inputs based on your eye angles
+		var wishSpeed = Input.Down( "Run" ) ? RunSpeed : WalkSpeed; // If we're running we use the running speed, else the walking speed
 
-		WishVelocity = 0;
+		WishVelocity = direction * wishSpeed; // The direction is normal, so we multiply its magnitude with the wish speed
+	}
 
-		if ( Input.Down( "Forward" ) ) WishVelocity += eyeRotation.Forward;
-		if ( Input.Down( "Backward" ) ) WishVelocity += eyeRotation.Backward;
-		if ( Input.Down( "Left" ) ) WishVelocity += eyeRotation.Left;
-		if ( Input.Down( "Right" ) ) WishVelocity += eyeRotation.Right;
+	public void ComputeHelper()
+	{
+		if ( CharacterController.IsOnGround ) // If we're touching the ground VVV
+		{
+			CharacterController.Velocity = CharacterController.Velocity.WithZ( 0 ); // Nullify any vertical velocity to stick to the ground
+			CharacterController.Accelerate( WishVelocity ); // Accelerate by our wish velocity
+			CharacterController.ApplyFriction( 4.0f ); // Make movements on ground responsive
+		}
+		else // If we're in air VVV
+		{
+			CharacterController.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta; // Apply the scene's gravity to the controller
+			CharacterController.Accelerate( WishVelocity.ClampLength( WalkSpeed / 2f ) ); // Give some control in air but not too much
+			CharacterController.ApplyFriction( 0.1f ); // Make movements in air slippery
+		}
 
-		WishVelocity = WishVelocity.WithZ( 0 );
+		CharacterController.Move(); // Move our character
+	}
 
-		if ( !WishVelocity.IsNearZeroLength )
-			WishVelocity = WishVelocity.Normal;
-
-		WishVelocity *= Input.Down( "Run" ) ? RunSpeed : WalkSpeed;
+	public void ComputeJump()
+	{
+		if ( CharacterController.IsOnGround ) // If you're on the ground
+			if ( Input.Down( "Jump" ) ) // And you're holding the JUMP button
+				CharacterController.Punch( Vector3.Up * JumpStrength ); // Make the player jump (Punch unsticks you from the ground and applies velocity)
 	}
 }
