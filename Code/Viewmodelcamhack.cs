68148fcbe7bd368e454af873defce40ee41c52ef
Retroeer <retro@halend.dev>
2024-11-23 22:00:56 -0500

    stop potential impossible NRE before it even happens

--- Code/Viewmodelcamhack.cs
+++ Code/Viewmodelcamhack.cs
@@ -1,15 +1,11 @@
-using Sandbox;
-
 public sealed class Viewmodelcamhack : Component
 {
 	protected override void OnStart()
 	{
-		GetComponent<CameraComponent>().ClearFlags = ClearFlags.Stencil | ClearFlags.Depth | ClearFlags.Color;
-		_ = Task.DelaySeconds(0.03f).ContinueWith(_ => { GetComponent<CameraComponent>().ClearFlags = ClearFlags.Stencil | ClearFlags.Depth; });
-	}
-
-	protected override void OnUpdate()
-	{
-
+		var camera = GetComponent<CameraComponent>();
+		if ( !camera.IsValid() ) 
+			return;
+		camera.ClearFlags = ClearFlags.Stencil | ClearFlags.Depth | ClearFlags.Color;
+		_ = Task.DelaySeconds(0.03f).ContinueWith(_ => { camera.ClearFlags = ClearFlags.Stencil | ClearFlags.Depth; });
 	}
 }
